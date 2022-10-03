93a822b79e273dcd0280f540b4453b9f04207a48
calexanderberg <carlalexanderberg@gmail.com>
2022-10-03 14:15:46 +0200

    updated the file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd "${dirname "${BASH_SOURCE}" )";
+cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin main;
 
@@ -10,7 +10,7 @@ function doIt() {
 		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
-		--exclude  "LICENSE-MIT.txt" \
+		--exclude "LICENSE-MIT.txt" \
 		-avh --no.perms . ~;
 	source ~/.bash_profile
 } 
@@ -19,7 +19,7 @@ if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt;
 else
 	read -p "This may overwrite existing files in your home direcory. Are you sure? (y/n) " -n 1;
-	echo "";
+	echo " ";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt;
 	fi
