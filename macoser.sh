8dc1a74feaa15d2cbc6a516fbc5f22924303b426
Alex <60197499+21st-centuryman@users.noreply.github.com>
2023-02-18 17:06:37 +0100

    Add: first proper version

--- macoser.sh
+++ macoser.sh
@@ -2,30 +2,31 @@
 # Install the script on a usb and run it.
 
 echo -e "Hello. Time to get the best goddamn setup script on the planet.\n"
-echo -e "Lets start with homebrew. The better package manager (f nix)\n"
+echo -e "Lets start with homebrew. The better package manager (f*** nix)\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo -e "homebrew installed... Downloading git to fetch Dotfiles.\n"
 
-brew install git
+brew install git stow
 
 echo -e "Git installation complete.\n"
 
-git clone https://github.com/21st-centuryman/dotfiles.git
+git clone https://github.com/21st-centuryman/dotfiles.git 
 
-echo -e "We will copy all these dot files to the .config directory for better management\n"
+echo -e "Stowing the files"
 
-# Do some command when im bothered
+cd dotfiles
+stow -t ~/.config/. /*
 
 echo -e "Sourcing all the directores that do not check the .config directories by default\n"
-echo -e "As of feb 2023, these files are git, tmux, and zsh\n"
+echo -e "As of feb 2023, these files are git, tmux, and zsh. We will source them using gnu stow\n"
 
-ln -s ~/.config/zsh/zshrc ~/.zshrc
-ln -s ~/.config/git/gitconfig ~/.gitconfig
-ln -s ~/.config/tmux/tmux.conf ~/.tmux.conf
+stow -t ~/. -d ~/.config/git/.gitconfig
+stow -t ~/. -d ~/.config/tmux/.tmux.conf
+stow -t ~/. -d ~/.config/zsh/.zshrc
 
-echo -e "Donwloading all CLI and gui apps (if you are on mac)"
+echo -e "Downloading all CLI and gui apps (if you are on mac)"
 
 brew bundle --file=~/.config/homebrew/Brewfile
 
