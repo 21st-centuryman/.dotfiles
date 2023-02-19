049b7061cde30e684d8c4e0521b3c6db081f80bc
calexanderberg <carlalexanderberg@gmail.com>
2023-02-19 20:30:09 +0100

    Fix: stow commands

--- macoser.sh
+++ macoser.sh
@@ -8,11 +8,12 @@ run_script () {
   echo -e "Stowing the files"
 
   cd dotfiles
-  stow -t ~/.config/. /*
+  stow -t ~/.config/. *
 
   echo -e "Sourcing all the directores that do not check the .config directories by default\n"
   echo -e "As of feb 2023, these files are git, tmux, and zsh. We will source them using gnu stow\n"
 
+  rm ~/.gitconfig ~/.zshrc ~/.tmux.conf
   stow -t ~/. -d ~/.config/git/.gitconfig
   stow -t ~/. -d ~/.config/tmux/.tmux.conf
   stow -t ~/. -d ~/.config/zsh/.zshrc
