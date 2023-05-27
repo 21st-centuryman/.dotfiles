8655f418a043ec667f060c01fa2267ed4d161b2a
21st <60197499+21st-centuryman@users.noreply.github.com>
2023-05-27 17:15:38 +0200

    Add: Create update.sh

---
+++ scripts/update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This is a script for updating the stuff for my server as soon as I have set it up using startup-server.sh
+# Yes it's only astetic, sue me.
+
+echo -e "Updating the server program"
+wget -P ~/.config/starship/ https://raw.githubusercontent.com/21st-centuryman/dotfiles/main/starship/starship.toml
+wget -P ~/.config/fish https://raw.githubusercontent.com/21st-centuryman/dotfiles/main/fish/config.fish
