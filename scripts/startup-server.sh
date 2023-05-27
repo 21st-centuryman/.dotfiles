40760b639ef7b09162af6c7a938e278cb9f1145b
21st <60197499+21st-centuryman@users.noreply.github.com>
2023-05-27 17:16:43 +0200

    Update startup-server.sh

--- scripts/startup-server.sh
+++ scripts/startup-server.sh
@@ -1,35 +1,14 @@
 #!/bin/bash
 
-update() {
-	wget -P ~/.config/starship/ https://raw.githubusercontent.com/21st-centuryman/dotfiles/main/starship/starship.toml
-	wget -P ~/.config/fish https://raw.githubusercontent.com/21st-centuryman/dotfiles/main/fish/config.fish
-}
-
-install() {
-	mkdir ~/.config ~/.config/fish ~/.config/starship
-
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
-	wget -P ~/.config/starship/ https://raw.githubusercontent.com/21st-centuryman/dotfiles/main/starship/starship.toml
-	wget -P ~/.config/fish https://raw.githubusercontent.com/21st-centuryman/dotfiles/main/fish/config.fish
-
-	cargo install pfetch starship
-}
+# Installs the starting configuration for my server.
 
 echo -e "Running startup server program."
 
-read -p -r "Do you want to update the current setup? Saying n will set up rust and install packages. [Y/n]" yn
+mkdir ~/.config ~/.config/fish ~/.config/starship
 
-while true; do
-	case $yn in
-	[Yy]*)
-		update
-		;;
-	[Nn]*)
-		install
-		;;
-	*)
-		read -p -r "Invalid response. Do you want to update your setup? [Y/n]" yn
-		;;
-	esac
-done
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+wget -P ~/.config/starship/ https://raw.githubusercontent.com/21st-centuryman/dotfiles/main/starship/starship.toml
+wget -P ~/.config/fish https://raw.githubusercontent.com/21st-centuryman/dotfiles/main/fish/config.fish
+
+cargo install pfetch starship
