001fbb562a88a6e01be07a595de9b8f961e48bbe
21st <60197499+21st-centuryman@users.noreply.github.com>
2023-08-23 14:08:51 +0200

    Fix: cargo install correct path

--- scripts/startup-server.sh
+++ scripts/startup-server.sh
@@ -4,9 +4,9 @@ echo -e "Running startup server program."
 
 mkdir ~/.config ~/.config/fish ~/.config/starship
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
 wget -P ~/.config/starship/ https://raw.githubusercontent.com/21st-centuryman/dotfiles/main/starship/starship.toml
 wget -P ~/.config/fish https://raw.githubusercontent.com/21st-centuryman/dotfiles/main/fish/config.fish
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 cargo install pfetch starship
