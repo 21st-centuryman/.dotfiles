f7f6be00c6a8b3d95579df24b24e46a2cb269952
calexanderberg <carlalexanderberg@gmail.com>
2023-09-15 21:05:44 +0200

    Add: more script stuff

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -54,28 +54,57 @@ done
 
 ### REWRITE BELOW ###
 
-os_deps() {
-	case "$1" in
+# ---------------------
+# Dependencies
+# ---------------------
+
+desktop_os_deps() {
+	case "$2" in
 	"darwin")
 		brew install ninja cmake gettext curl # Neovim
 		brew install curl                     # Cargo packages
+		brew install stow                     # Finalization step
 		;;
 	"openbsd")
 		pkg_add gmake cmake unzip curl gettext-tools # Neovim
 		pkg_add curl                                 # Cargo packages
+		pkg_add stow                                 # Finalization step
 		;;
 	"artix")
 		pacman -S base-devel cmake unzip ninja curl # Neovim
 		pacman -S curl                              # Cargo packages
+		pacman -S stow                              # Finalization step
 		;;
 	"alpine")
 		apk add build-base cmake coreutils curl unzip gettext-tiny-dev # Neovim
 		apk add curl                                                   # Cargo packages
+		apk add stow                                                   # Finalization step
 		;;
 	# Add more cases for other operating systems as needed
 	*) ;;
 	esac
 }
+
+server_os_deps() {
+	case "$2" in
+	"debian")
+		apt-get install curl                                    # Rust
+		apt-get install libgit2-dev libcurl4 libssh-dev pkgconf # cargo-update
+		apt-get install wget                                    # Finalization step
+		;;
+	"alpine")
+		apk add curl                                                   # Rust
+		apk add build-base cmake coreutils curl unzip gettext-tiny-dev # cargo-update???? EDIT
+		apk add wget                                                   # Finalization step
+		;;
+	# Add more cases for other operating systems as needed
+	*) ;;
+	esac
+}
+
+# ---------------------
+# BUILDING FROM SOURCE
+# ---------------------
 
 build_nvim() {
 	local build_command="make"
@@ -108,4 +137,20 @@ build_st() {
 		make clean install
 	fi
 	cd ../../../
+}
+
+# ---------------------
+# stow and finalization
+# ---------------------
+
+desktop_final() {
+	stow -t ~/.config/. ../.
+	rm ~/.gitconfig ~/.tmux.conf
+	ln -s ~/.config/git/gitconfig ~/.gitconfig
+	ln -s ~/.config/tmux/tmux.conf ~/.tmux.conf
+}
+
+server_final() {
+	wget -P ~/.config/starship/ https://raw.githubusercontent.com/21st-centuryman/dotfiles/main/starship/starship.toml
+	wget -P ~/.config/fish https://raw.githubusercontent.com/21st-centuryman/dotfiles/main/fish/config.fish
 }
